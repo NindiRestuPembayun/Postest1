1c3d10b64e7d787e94cf509ef3db65ceaad7edfa
NindiRestuPembayun <92658873+NindiRestuPembayun@users.noreply.github.com>
2021-11-21 20:39:05 +0700

    Add files via upload

---
+++ 2117051021_ Nindi Restu Pembayun_ Posttest1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	string Nama;
+	string NPM;
+	string Alamat;
+	cout<<"Nama  = ";
+	getline(cin,Nama);
+	cout<<"NPM   = ";
+	getline(cin,NPM);
+	cout<<"Alamat = ";
+	getline(cin,Alamat);
+	cout<<"Nama Saya Adalah "<<Nama<<" NPM "<<NPM<<" Alamat "<<Alamat<<endl<<">\\\\<"<<endl<<"\""<<Nama<<"\"";
+	
+	return 0;
+	
+	
+}
